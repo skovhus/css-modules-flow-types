ed07c604af4c7613ef69e283a1663349fb43f140
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2017-06-18 22:49:37 +0200

    fix: also emit empty .css.flow files (closes #2)

--- packages/css-modules-flow-types-loader/index.js
+++ packages/css-modules-flow-types-loader/index.js
@@ -8,16 +8,16 @@ function getTokens(content) {
   if (cssTokens) {
     return JSON.parse(cssTokens[1]);
   }
-  return null;
+  return {};
 }
 
 module.exports = function cssModulesFlowTypesLoader(content) {
   const tokens = getTokens(content);
-  if (tokens) {
-    // NOTE: We cannot use .emitFile as people might use this with devServer
-    // (e.g. in memory storage).
-    const outputPath = this.resourcePath + '.flow';
-    fs.writeFile(outputPath, printFlowDefinition(tokens), {}, function() {});
-  }
+
+  // NOTE: We cannot use .emitFile as people might use this with devServer
+  // (e.g. in memory storage).
+  const outputPath = this.resourcePath + '.flow';
+  fs.writeFile(outputPath, printFlowDefinition(tokens), {}, function() {});
+
   return content;
 };
