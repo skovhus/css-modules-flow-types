a08308218e22f141041a237718a458e217b9394f
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2017-10-13 23:48:36 +0200

    refactor: remove dead code from fileSystemLoader

--- packages/css-modules-flow-types-cli/src/css-modules/fileSystemLoader.js
+++ packages/css-modules-flow-types-cli/src/css-modules/fileSystemLoader.js
@@ -15,23 +15,6 @@ import path from 'path';
 
 import Core from 'css-modules-loader-core';
 
-// Sorts dependencies in the following way:
-// AAA comes before AA and A
-// AB comes after AA and before A
-// All Bs come after all As
-// This ensures that the files are always returned in the following order:
-// - In the order they were required, except
-// - After all their dependencies
-const traceKeySorter = (a, b) => {
-  if (a.length < b.length) {
-    return a < b.substring(0, a.length) ? -1 : 1;
-  } else if (a.length > b.length) {
-    return a.substring(0, b.length) <= b ? -1 : 1;
-  } else {
-    return a < b ? -1 : 1;
-  }
-};
-
 export default class FileSystemLoader {
   constructor(root, plugins) {
     this.root = root;
@@ -90,17 +73,5 @@ export default class FileSystemLoader {
           }, reject);
       }
     });
-  }
-
-  get finalSource() {
-    return Object.keys(this.sources)
-      .sort(traceKeySorter)
-      .map(s => this.sources[s])
-      .join('');
-  }
-
-  clear() {
-    this.tokensByFile = {};
-    return this;
   }
 }
