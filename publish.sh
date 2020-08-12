dbfcf77fb8577863c7a8d855ea20f6508ff51b6b
skovhus <kenneth.skovhus@gmail.com>
2020-08-12 19:33:19 +0200

    chore: fix publishing order

--- publish.sh
+++ publish.sh
@@ -15,7 +15,10 @@ fi
 yarn install
 yarn run verify:bail
 
-yarn lerna exec npm publish
+# Until we enforce semantic commits (and can use lerna publish), we need to publish in the right order
+# cd packages/css-modules-flow-types-printer && npm publish && cd ../../
+cd packages/css-modules-flow-types-cli && npm publish && cd ../../
+cd packages/css-modules-flow-types-loader && npm publish && cd ../../
 
 git tag -a "${tag}" -m "${tag}"
 git push origin "${tag}"
