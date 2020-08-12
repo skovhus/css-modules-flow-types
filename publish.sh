45f20364f183566a2a512b35e4fe1958540c0842
skovhus <kenneth.skovhus@gmail.com>
2020-08-12 12:10:08 +0200

    chore: setup CI deployment

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version=$(cat lerna.json | jq -r .version)
+tag="v${version}"
+
+publishedVersion=$(yarn info css-modules-flow-types-cli --json | jq -r .data.\"dist-tags\".latest)
+
+if [ "$version" = "$publishedVersion" ]; then
+    echo "Newest version is already deployed."
+    exit 0
+fi
+
+yarn run clean
+yarn install
+yarn run verify:bail
+
+npm publish
+
+git tag -a "${tag}" -m "${tag}"
+git push origin "${tag}"
